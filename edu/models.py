2ed5370c557637b1f44a2626d9898940623e7d6c
Chaehyeon <chaehyeon119>
2023-01-24 20:47:29 +0900

    첨부 파일 있을 경우 포스트 상세 페이지에 다운로드 버튼 출력하기

--- edu/models.py
+++ edu/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 # Create your models here.
 
@@ -16,6 +17,12 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/edu/{self.pk}/'
+    
+    def get_file_name(self): #파일명
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self): #확장자
+        return self.get_file_name().split('.')[-1]
 
 # class Post(models.Model):
 #     title = models.CharField(max_length=30)
