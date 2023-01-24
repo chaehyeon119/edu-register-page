fec40bd5ca62dce174e840ae6f56bbc189e28464
Chaehyeon <chaehyeon119>
2023-01-24 17:07:33 +0900

    Post 모델을 생성하고 시간 설정하기

--- edu/models.py
+++ edu/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}' #해당 포스트의 pk값과 title이 나옴 ex [1]첫번째 포스트
+
+# class Post(models.Model):
+#     title = models.CharField(max_length=30)
+#     hook_text = models.CharField(max_length=100, blank=True)
+#     content = MarkdownxField()
+
+#     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+#     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+#     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+#     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+#     tags = models.ManyToManyField(Tag, blank=True)
+
+#     def __str__(self):
+#         return f'[{self.pk}]{self.title} :: {self.author}'
+
+#     def get_absolute_url(self):
+#         return f'/blog/{self.pk}/'
+
+#     def get_file_name(self):
+#         return os.path.basename(self.file_upload.name)
+
+#     def get_file_ext(self):
+#         return self.get_file_name().split('.')[-1]
+
+#     def get_content_markdown(self):
+#         return markdown(self.content)
+
+#     def get_avatar_url(self):
+#         if self.author.socialaccount_set.exists():
+#             return self.author.socialaccount_set.first().get_avatar_url()
+#         else:
+#             return f'https://api.adorable.io/avatars/60/{self.author.username}.png'
+
